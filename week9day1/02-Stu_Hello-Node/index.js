163278bdc36aa086b4866488832a434c131b7712
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-01-23 20:34:57 -0500

    Completed activity 2 stu_hello-node exercise. Ran through bonus material

---
+++ week9day1/02-Stu_Hello-Node/index.js
@@ -0,0 +1,12 @@
+console.log("Hellooooo Node!!!!!");
+
+//Bonus
+
+//console.log(window); // error window is not defined
+
+//console.log(prompt); // error prompt is not defined
+
+//console.log(alert) // error alert is not defined
+
+//console.log(confirm) // error confirm is not defined
+
