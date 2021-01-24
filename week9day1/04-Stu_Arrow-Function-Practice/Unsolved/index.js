8a4e2f7ad3a3cf5a9639194f086d06be5b05668d
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-01-24 11:27:50 -0500

    Corrected functions in Item 4 of Exercise 4

--- week9day1/04-Stu_Arrow-Function-Practice/Unsolved/index.js
+++ week9day1/04-Stu_Arrow-Function-Practice/Unsolved/index.js
@@ -63,13 +63,13 @@ var netflixQueue = {
     "Eternal Sunshine of the Spotless Mind",
     "Fight Club"
   ],
-  watchMovie: () => {
+  watchMovie: function() {
     this.queue.pop();
   },
-  addMovie: (movie) => {
+  addMovie: function(movie){
     this.queue.unshift(movie);
   },
-  printQueue: () => {
+  printQueue: function() {
     var list = "";
     for (var i = this.queue.length - 1; i >= 0; i--) {
       var currentMovie = this.queue[i];
