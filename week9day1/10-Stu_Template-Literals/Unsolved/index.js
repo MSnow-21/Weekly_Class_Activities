266ae2e8432b291094e6908527cf9aae3d20d561
Jordan Fly <73404951+MSnow-21@users.noreply.github.com>
2021-01-27 16:31:34 -0500

    Reviewed exercises. Completed exercise 10.

--- week9day1/10-Stu_Template-Literals/Unsolved/index.js
+++ week9day1/10-Stu_Template-Literals/Unsolved/index.js
@@ -1,16 +1,40 @@
 // music should be an object with title, artist, and album properties
 const music = {
   // code here
+  title: "Genesis",
+  artist: "Deftones",
+  album: "rock",
 };
 
 // Write code between the <div> tags to output the data from the music object above.
 // Use an h2 element for the title and a p element for artist and title
 const songSnippet = `
   <div class="song">
-
+  <h2>Album title is: ${music.title}</h2>
+  <p>Artist is: ${music.artist}</p>
+  <p>Album type is: ${music.album}</p>
   </div>
 `;
 
 const element = document.getElementById("music");
 element.innerHTML = songSnippet;
+
+//testing another style using text Content
+
+const elementTwo = document.getElementById("musicTwo");
+
+const songSnippetH2 = document.createElement("h2");
+const songSnippetP1 = document.createElement("p");
+const songSnippetP2 = document.createElement("p");
+
+songSnippetH2.textContent = `Album title is: ${music.title}`;
+elementTwo.appendChild(songSnippetH2);
+
+songSnippetP1.textContent = `Artist is: ${music.artist}`;
+elementTwo.appendChild(songSnippetP1);
+
+songSnippetP2.textContent = `Album is: ${music.album}`;
+elementTwo.appendChild(songSnippetP2);
+
+
 
